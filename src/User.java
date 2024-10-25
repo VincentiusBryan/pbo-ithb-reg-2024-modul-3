8d6c241b1480b4e74b78ef6493df0cec732bac12
VincentiusBryan <if-23018@students.ithb.ac.id>
2024-10-25 14:31:32 +0700

    User

--- src/User.java
+++ src/User.java
@@ -11,14 +11,31 @@ public abstract class User {
         this.telepon = telepon;
     }
 
-    public String getNama() { return nama; }
-    public void setNama(String nama) { this.nama = nama; }
-    public String getAlamat() { return alamat; }
-    public void setAlamat(String alamat) { this.alamat = alamat; }
-    public String getTtl() { return ttl; }
-    public void setTtl(String ttl) { this.ttl = ttl; }
-    public String getTelepon() { return telepon; }
-    public void setTelepon(String telepon) { this.telepon = telepon; }
+    public String getNama() { 
+        return nama; 
+    }
+
+    public void setNama(String nama) { 
+        this.nama = nama; 
+    }
+    public String getAlamat() { 
+        return alamat; 
+    }
+    public void setAlamat(String alamat) { 
+        this.alamat = alamat; 
+    }
+    public String getTtl() { 
+        return ttl; 
+    }
+    public void setTtl(String ttl) { 
+        this.ttl = ttl; 
+    }
+    public String getTelepon() { 
+        return telepon; 
+    }
+    public void setTelepon(String telepon) { 
+        this.telepon = telepon; 
+    }
 
     @Override
     public abstract String toString();
